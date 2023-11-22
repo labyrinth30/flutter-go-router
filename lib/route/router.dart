c5b22c3282309c9115d259d22998ee03fbb1c9c7
Lee, Younha <younha0088@gmail.com>
2023-11-23 02:12:13 +0900

    Logging

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -168,4 +168,5 @@ final router = GoRouter(
   errorBuilder: (context, state) => ErrorScreen(
     error: state.error.toString(),
   ),
+  debugLogDiagnostics: true,
 );
--- lib/screens/11_error_screen.dart
+++ lib/screens/11_error_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_go_router/common/custom_button.dart';
-import 'package:flutter_go_router/layout/default_layout.dart';
 import 'package:go_router/go_router.dart';
 
 class ErrorScreen extends StatelessWidget {
